c6eae8f62acf7f63fdef97aaf9e458224a49f893
Achraf Chouk <ach@takeatea.com>
2013-07-11 03:35:12 +0200

    fix bug in image field template

--- tpl/fields/__field_image.tpl.php
+++ tpl/fields/__field_image.tpl.php
@@ -12,21 +12,22 @@
 
                         <div class="inside image image-<?php echo $type ?>">
                             <fieldset>
-                                <?php foreach ($options as $option): ?>
+                                <?php $i = 1 ?>
+                                <?php foreach ($options as $key => $option): ?>
                                     <?php
-                                        $selected = is_array($val) && in_array($option, $val) ? true : ($option == $val ? true : false);
-                                        $pathinfo = pathinfo($option);
-                                        $for = $id . '_' . $pathinfo['filename'];
-                                        $name = $multiple ? $id . '[' . $pathinfo['filename'] . ']' : $id;
+                                        $selected = is_array($val) && in_array($key, $val) ? true : ($key == $val ? true : false);
+                                        $for = $id . '_' . $i;
+                                        $name = $multiple ? $id . '[' . $option . ']' : $id;
+                                        $i++;
                                     ?>
                                     <label for="<?php echo $for ?>" class="<?php echo $selected ? 'selected' : '' ?>">
                                         <div class="image-to-show">
-                                            <img src="<?php echo $option ?>" width="<?php echo $width ?>" height="<?php echo $height ?>" />
+                                            <img src="<?php echo $key ?>" width="<?php echo $width ?>" height="<?php echo $height ?>" />
                                             <span>
-                                                <?php echo $pathinfo['filename'] ?>
+                                                <?php echo $option ?>
                                             </span>
                                         </div>
-                                        <input type="<?php echo $type ?>" name="<?php echo $name ?>" id="<?php echo $for ?>" value="<?php echo $option ?>" <?php echo $selected ? 'checked="checked" ' : '' ?> />
+                                        <input type="<?php echo $type ?>" name="<?php echo $name ?>" id="<?php echo $for ?>" value="<?php echo $key ?>" <?php echo $selected ? 'checked="checked" ' : '' ?> />
                                     </label>
                                 <?php endforeach ?>
                             </fieldset>
