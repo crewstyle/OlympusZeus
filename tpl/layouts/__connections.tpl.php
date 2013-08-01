e9bc6ef9129fef2fe14c8c58efebc2beb3f46823
Achraf Chouk <ach@takeatea.com>
2013-08-01 02:20:35 +0200

    change order display on connection page

--- tpl/layouts/__connections.tpl.php
+++ tpl/layouts/__connections.tpl.php
@@ -12,6 +12,11 @@ $details = array(
         'title' => __('Simple, use these boxes to fully integrate your Tea Theme Options for Wordpress with your favorites social networks.')
     ),
     array(
+        'type' => 'twitter',
+        'title' => __('Twitter.'),
+        'description' => __('Login to your Twitter profile to get all your photos.')
+    ),
+    array(
         'type' => 'instagram',
         'title' => __('Instagram.'),
         'description' => __('Login to your Instagram profile to get all your photos.')
@@ -20,10 +25,5 @@ $details = array(
         'type' => 'flickr',
         'title' => __('FlickR.'),
         'description' => __('Login to your FlickR profile to get all your photos.')
-    ),
-    array(
-        'type' => 'twitter',
-        'title' => __('Twitter.'),
-        'description' => __('Login to your Twitter profile to get all your photos.')
     )
 );
