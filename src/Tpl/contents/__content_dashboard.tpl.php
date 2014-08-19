7b6f5684ee8f2c38d50a912329aa2a5d544334be
Ach <ach@takeatea.com>
2014-08-19 12:21:25 +0200

    update dashboard texts

--- src/Tpl/contents/__content_dashboard.tpl.php
+++ src/Tpl/contents/__content_dashboard.tpl.php
@@ -22,6 +22,7 @@ $details = array(
                 <li><b>Transient API</b> - Very similar to the Options API but with the added feature of an expiration time, which simplifies the process of using the wp_options database table to temporarily store cached information.</li>
                 <li><b>Easier for administrators</b> - The interface is thought to be the most userfriendly. The Tea TO core adds some extra interface customisations to make your life easier.</li>
                 <li><b>Easier for developers</b> - Create a new admin panel easily with the new dashboard. The Tea TO core is made to allow non-developer profiles to easily create the settings they need to customise their templates.</li>
+                <li><b>Elasticsearch API</b> - Elasticsearch creates scaleable, real-time search for your website by indexing all your datas millions of times a day.</li>
             </ul>', TTO_I18N)
     ),
     array(
@@ -30,6 +31,8 @@ $details = array(
         'position' => 'right',
         'content' => __('<h2>The <b>Tea T.O.</b> is built for <a href="http://wordpress.org" target="_blank">Wordpress</a> v3.x and uses the Wordpress built-in pages.</h2>
             <ul>
+                <li><b>Custom Post Types</b> - Here is the simpliest way to create Wordpress Custom Post Types! A new experience with dashicons, complete customisable backend panels, custom fields and datas saved in DB as the good way.</li>
+                <li><b>Custom Taxonomies</b> - Since the 1.4.2 version, you are now able to create easily Wordpress Custom Taxonomies with a couple of lines code. The Tea TO permit to add custom fields as you want.</li>
                 <li><b>Wordpress Media Manager</b> - Beautiful interface: A streamlined, all-new experience where you can create galleries faster with drag-and-drop reordering, inline caption editing, and simplified controls.</li>
                 <li><b>Full of Options</b> - 3 kind of options used to display information, store fields values or get data from your Wordpress database. The options are made to build your Wordpress pages easily.</li>
             </ul>', TTO_I18N)
