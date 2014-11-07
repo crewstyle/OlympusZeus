200eddbce156385f19e4a165ba3bab8fab9ab8ec
crewstyle <ach@takeatea.com>
2014-11-07 17:02:39 +0100

    Fix bugs on <upload> field for galleries

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,8 @@
+## 1.5.0-2 (November 07, 2014)
+
+Fix bugs on <upload> field for galleries
+
+
 ## 1.5.0-1 (November 07, 2014)
 
 Fix bugs on <upload> field, add some CSS enhancements
--- src/Fields/Upload/Upload.php
+++ src/Fields/Upload/Upload.php
@@ -32,7 +32,7 @@ if (!defined('ABSPATH')) {
  *
  * @package Tea Fields
  * @subpackage Tea Fields Upload
- * @since 1.5.0
+ * @since 1.5.0-2
  *
  */
 class Upload extends TeaFields
@@ -59,7 +59,7 @@ class Upload extends TeaFields
      * @param array $content Contains all data
      * @param array $post Contains all post data
      *
-     * @since 1.5.0
+     * @since 1.5.0-2
      */
     public function templatePages($content, $post = array(), $prefix = '')
     {
@@ -84,6 +84,7 @@ class Upload extends TeaFields
         $delete = __('Delete selection', TTO_I18N);
         $wplink = includes_url() . 'images/media/document.png';
 
+
         //Default way
         if (empty($post)) {
             //Check selected
@@ -93,8 +94,31 @@ class Upload extends TeaFields
         //On CPT
         else {
             //Check selected
-            $vals = get_post_meta($post->ID, $post->post_type . '-' . $id, false);
-            $vals = empty($vals) ? $std : (is_array($vals) ? $vals[0] : array($vals));
+            $oldv = get_post_meta($post->ID, $post->post_type . '-' . $id, false);
+            $vals = array();
+
+            //Retro-compatibility
+            if (isset($oldv[0]) && is_string($oldv[0])) {
+                $oldv = !empty($oldv) ? explode('||', $oldv[0]) : array();
+
+                //Create the TeaTO index 0
+                $vals[] = array('url' => '', 'id' => '', 'name' => '');
+
+                //Iterate on old values
+                foreach ($oldv as $item) {
+                    $it = explode(';', $item);
+                    $vals[] = array(
+                        'url' => $it[1],
+                        'id' => $it[0],
+                        'name' => ''
+                    );
+                }
+            }
+            else {
+                $vals = $oldv;
+            }
+
+            //$vals = empty($vals) ? $std : (is_array($vals) ? $vals[0] : array($vals));
         }
 
         //Fix array bug
